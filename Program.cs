5ae19ec2a7a2f4ce7574000e0d2c0dc104ef96c5
User11 <frolovaea27@gmail.com>
2023-03-30 11:21:16 +0300

    dz 47 50 52

--- Program.cs
+++ Program.cs
@@ -1,357 +1,158 @@
-﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
-// Напишите программу, которая покажет количество чётных чисел в массиве.
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 
-// [345, 897, 568, 234] -> 2
+// m = 3, n = 4.
 
-// Console.WriteLine("Необходимо указать размер массива");
-// int size = Convert.ToInt32(Console.ReadLine());
-// int[] numbers = new int[size];
+// 0,5 7 -2 -0,2
 
-// FillArray(numbers);
-// Console.WriteLine("Анализируемый массив: ");
-// PrintArray(numbers);
-// int count = 0;
+// 1 -3,3 8 -9,9
 
-// for (int j = 0; j < numbers.Length; j++)
-// if (numbers[j] % 2 == 0)
-// count++;
+// 8 7,8 -7,1 9
 
-// Console.WriteLine($"Из {numbers.Length} чисел, {count} являются четными");
+// int m = ReadInt("Введите количество строк матрицы: ");
+// int n = ReadInt("Введите количество столбцов матрицы: ");
+// double [,] matrix = new double [m, n];
+// CreateMatrix(matrix);
+// PrintMatrix(matrix);
 
-// void FillArray(int[] numbers)
+// int ReadInt(string text)
 // {
-//     for(int i = 0; i < numbers.Length; i++)
+//     System.Console.Write(text);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+//  void CreateMatrix(double [,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
 //     {
-//         numbers[i] = new Random().Next(100,1000);
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i,j] = new Random().Next(-10, 11) / 10.0;
+//         }
 //     }
+
 // }
-// void PrintArray(int[] numbers)
+// void PrintMatrix(double [,] matrix)
 // {
-//     for(int i = 0; i < numbers.Length; i++)
+//     for (int i = 0; i < matrix.GetLength(0); i++)
 //     {
-//         Console.Write(numbers[i] + " ");
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             System.Console.Write(matrix[i,j] + "\t");
+//         }
+//         System.Console.WriteLine();
 //     }
-//     Console.WriteLine();
 // }
 
 
-// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
-// Найдите сумму элементов, стоящих на нечётных позициях.
+// Задача 50. Напишите программу, которая на вход принимает значение элемента в двумерном массиве, 
+// и возвращает позицию этого элемента или же указание, что такого элемента нет.
 
-// [3, 7, 23, 12] -> 19
+// Например, задан массив:
 
-// [-4, -6, 89, 6] -> 0
+// 1 4 7 2
 
-// Console.WriteLine("Необходимо указать размер массива");
-// int size = Convert.ToInt32(Console.ReadLine());
-// int[] numbers = new int[size];
+// 5 9 2 3
 
-// FillArray(numbers);
-// Console.WriteLine("Анализируемый массив: ");
-// PrintArray(numbers);
-// int sum = 0;
+// 8 4 2 4
 
-// for (int j = 0; j < numbers.Length; j+=2)
-//     sum = sum + numbers[j];
+// 17 -> такого числа в массиве нет
 
-//     Console.WriteLine($"в массиве чисел {numbers.Length}, сумма элементов из нечетных позиций = {sum}");
+// int [,] matrix = new int [6, 8];
+// CreateMatrix(matrix);
+// PrintMatrix(matrix);
+// GetPositionOfElement(matrix);
 
-// void FillArray(int[] numbers)
+// void GetPositionOfElement(int[,] matrix)
 // {
-//     for(int i = 0; i < numbers.Length; i++)
-//         {
-//             numbers[i] = new Random().Next(1,10);
-//         }
+// Console.WriteLine("Введите значение элемента a: ");
+// var a = int.Parse(Console.ReadLine()!);
+// for (var i = 0; i < matrix.GetLength(0); i++)
+// {
+// for (var j = 0; j < matrix.GetLength(1); j++)
+// {
+// if (matrix[i, j].Equals(a))
+// {
+// Console.WriteLine("Позиция этого элемента: " + (i, j));
+// return;
 // }
-// void PrintArray(int[] numbers)
-// {
-//     for(int i = 0; i < numbers.Length; i++)
-//         {
-//             Console.Write(numbers[i] + " ");
-//         }
-//     Console.WriteLine();
 // }
-
-
-// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и
-//  минимальным элементов массива.
-
-// [3 7 22 2 78] -> 76
-
-// Console.WriteLine("Необходимо указать размер массива");
-// int size = Convert.ToInt32(Console.ReadLine());
-// double[] numbers = new double[size];
-// FillArray(numbers);
-// Console.WriteLine("Анализируемый массив: ");
-// PrintArray(numbers);
-// double min = Int32.MaxValue;
-// double max = Int32.MinValue;
-
-// for (int j = 0; j < numbers.Length; j++)
-// {
-//     if (numbers[j] > max)
-//         {
-//             max = numbers[j];
-//         }
-//     if (numbers[j] < min)
-//         {
-//             min = numbers[j];
-//         }
+// }
+// Console.WriteLine("Такого элемента нет");
 // }
 
-// Console.WriteLine($"в массив чисел {numbers.Length} чисел. Максимум = {max}, минимум = {min}");
-// Console.WriteLine($"разница между максимум и минимум значением = {max - min}");
-
-// void FillArray(double[] numbers)
+//  void CreateMatrix(int [,] matrix)
 // {
-//     for(int i = 0; i < numbers.Length; i++)
-//         {
-//             numbers[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
-//         }
-// }
-// void PrintArray(double[] numbers)
-// {
-//     for(int i = 0; i < numbers.Length; i++)
-//         {
-//             Console.Write(numbers[i] + " ");
-//         }
-    
-//     Console.WriteLine();
-// }
-
-
-
-
-
-
-// // Задача 19
-
-// // Напишите программу, которая принимает на вход пятизначное число и проверяет, 
-// // является ли оно палиндромом. Через строку решать нельзя.
-
-// // 14212 -> нет
-
-// // 12821 -> да
-
-// // 23432 -> да
-
-//  void palindrom()
-//         {
-//         //int number = new Random().Next(1, 100000); for random number!!
-
-//         Console.Write("необходимо ввести пятизначное число");
-//         int number = Convert.ToInt32(Console.ReadLine()); 
-//         int number1 = number / 10000 % 10;
-//         int number2 = number / 1000 % 10;
-//         int reverse1 = number / 10 % 10;
-//         int reverse2 = number % 10;
-//         int noll = number / 10000;
-        
-//             if (noll < 1 || noll > 9){
-//             Console.WriteLine(number + "введено не пятизначное число");
-//             }
-//             else if  (number1 == reverse2 && number2 == reverse1){
-//             Console.WriteLine(number + "Введенное число - палиндром");
-//             }
-//             else
-//             {
-//             Console.WriteLine(number + "Введенное число не палиндром");
-//             }
-            
-//         }
-//         for (int i = 0; i < 10; i++)
-//         { 
-//             palindrom();
-//             break;
-//     }
-
-// Задача 21
-
-// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между 
-// ними в 3D пространстве.
-
-// A (3,6,8); B (2,1,-7), -> 15.84
-
-// A (7,-5, 0); B (1,-1,9) -> 11.53
-
-// Console.WriteLine("Введите x 1 точки");
-
-// int x1 = Convert.ToInt32(Console.ReadLine());
-
-// Console.WriteLine("Введите y 1 точки");
-
-// int y1 = Convert.ToInt32(Console.ReadLine());
-
-// Console.WriteLine("Введите z 1 точки");
-
-// int z1 = Convert.ToInt32(Console.ReadLine());
-
-// Console.WriteLine("Введите x 2 точки");
-
-// int x2 = Convert.ToInt32(Console.ReadLine());
-
-// Console.WriteLine("Введите y 2 точки");
-
-// int y2 = Convert.ToInt32(Console.ReadLine());
-
-// Console.WriteLine("Введите z 2 точки");
-
-// int z2 = Convert.ToInt32(Console.ReadLine());
-
-// Console.WriteLine(DistancePount(x1, y1, x2, y2, z1, z2));
-
-// // теорема пифагора d = Vsqrt((x2-x1)^2 + (y2-y1)^2 + (z1-z2)^2)
-
-// double DistancePount(int x1, int y1, int x2, int y2, int z1, int z2)
-
-// {
-
-//     double result1 = (x2 - x1)*(x2 - x1);
-//     double result2 = (y2 - y1)*(y2 - y1);
-//     double result3 = (z2 - z1)*(z2 - z1);
-
-//     double result = Math.Sqrt(result1 + result2 + result3);
-
-//     return result;
-// }
-
-// Задача 23
-
-// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-
-// 3 -> 1, 8, 27
-// 5 -> 1, 8, 27, 64, 125
-
-//  Console.Write("Введите число: ");
-//  int number = Convert.ToInt32(Console.ReadLine());
-
-// for (int i = 1; i <= number; i++)
-// {
-//     Console.Write($"{i * i * i}, ");
-// }
-
-
-
-
-
-
-
-
-
-// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
-//какое число большее, а какое меньшее.
-
-// a = 5; b = 7 -> max = 7
-// a = 2 b = 10 -> max = 10
-// a = -9 b = -3 -> max = -3
-
-// Console.WriteLine("Введите первое число");
-// int a = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите второе число");
-// int b = Convert.ToInt32(Console.ReadLine());
-// if( a == b)
-// {
-//     Console.WriteLine("Числа равны");
-// }
-
-// else if( a > b)
-// {
-//     Console.WriteLine("Первое число " +a + " больше второго числа " +b);
-
-// }
-// else Console.WriteLine("Второе число " +b + " больше первого числа " +a);
-
-// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт 
-//максимальное из этих чисел.
-
-// 2, 3, 7 -> 7
-// 44 5 78 -> 78
-// 22 3 9 -> 22
-
-// Console.WriteLine("Введите первое число");
-// int a = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите второе число");
-// int b = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите третье число");
-// int c = Convert.ToInt32(Console.ReadLine());
-
-// int max = a;
-
-// if( b > max)
-// {
-// max = b;
-// }
-// if ( c > max)
-// {
-//     max = c;
-// }
-// Console.WriteLine("Наибольшим из трех введенных чисел является " + max);
-
-
-// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
-//является ли число чётным (делится ли оно на два без остатка).
-
-// 4 -> да
-// -3 -> нет
-// 7 -> нет
-
-// Console.WriteLine("Введите число");
-// int a = Convert.ToInt32(Console.ReadLine());
-
-// if(a%2 == 1)
-// {
-//     Console.WriteLine("Введенное число - нечетное");
-// }
-// else
-// {
-//     Console.WriteLine("Введенное число - четное");
-// }
-
-// Задача 8: Напишите программу, которая на вход принимает число (N), 
-//а на выходе показывает все чётные числа от 1 до N.
-
-// 5 -> 2, 4
-// 8 -> 2, 4, 6, 8
-
-// int n = 1;
-// bool not = true;
-
-// Console.WriteLine("Введите число");
-// int a = Convert.ToInt32(Console.ReadLine());
-
-// Console.Write("Все четные числа от 1 до " +a + " это: ");
-// while (n <= a)
-// {
-//     if (n%2 != 1)
+//     for (int i = 0; i < matrix.GetLength(0); i++)
 //     {
-//         Console.Write(n + ",");
-//         not = false;
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i,j] = new Random().Next(-10, 11);
+//         }
 //     }
-//     n++;
+
 // }
-// if(not)
+// void PrintMatrix(int [,] matrix)
 // {
-//     Console.WriteLine(" Нет четных чисел от 1 до " +a);
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             System.Console.Write(matrix[i,j] + "\t");
+//         }
+//         System.Console.WriteLine();
+//     }
 // }
 
-// Задача 7 HARD по желанию - идет за 2 необязательных Напишите программу, 
-//которая принимает на вход целое число любой разрядности и на выходе показывает
-// вторую цифру слева этого числа или говорит, что такой цифры нет. 
-//Через строку решать нельзя.
-// 456111 -> 5
-// 78 -> 8
-// 9146548 -> 1
-// 3 -> нет
 
-//     Console.WriteLine("Введите число");
-//     int n = Convert.ToInt32(Console.ReadLine());
-//     int d = OurSecondDigit(n);
-//     Console.WriteLine(d);
-//     Console.ReadKey();
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в 
+// каждом столбце.
 
-// int OurSecondDigit(int k)
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+// int n = 3;
+// int m = 4;
+// int [,] matrix = new int [n, m];
+// CreateMatrix(matrix);
+// PrintMatrix(matrix);
+
+// for (int j = 0; j < matrix.GetLength(1); j++)
 // {
-//     while (k >=100) k /= 10;
-//     int d = k%10;
-//     return d;
+//     double result = 0.0;
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+//     result += matrix[i, j];
 // }
+// result = result / n;
+// Console.Write(result + "; ");
+// }
+// Console.WriteLine();
+
+
+
+//  void CreateMatrix(int [,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i,j] = new Random().Next(1, 11);
+//         }
+//     }
+
+// }
+// void PrintMatrix(int [,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             System.Console.Write(matrix[i,j] + "\t");
+//         }
+//         System.Console.WriteLine();
+//     }
+// }
+
